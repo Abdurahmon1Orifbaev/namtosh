83a0ec5ae92b8b314e0190e269867ce0eea57375
Abdurahmon1Orifbaev <abdurahmonorifboev74@gmail.com>
2025-02-23 11:41:11 +0500

    Config changed

--- data/config.py
+++ data/config.py
@@ -5,3 +5,6 @@ env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")
 ADMINS = env.list("ADMINS")
+import os
+
+GROUP_CHAT_ID = int(os.getenv('GROUP_CHAT_ID', '-1002457389396'))  # Fallback ID as an example
